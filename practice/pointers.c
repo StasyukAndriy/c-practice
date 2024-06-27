c07de4282206bf69efc05e57096779c29fe4c780
StasyukAndriy <stasyuk.andrii.b@gmail.com>
2024-06-27 17:13:17 +0300

    update

--- practice/pointers.c
+++ practice/pointers.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
 int main (){
-    int x = 1086;
-    int *pX = &x;
-    // printf("asas%d", *pX);
-    char *p0X = (char*)pX;
-    // printf("qwqwqw%d", *(p0X+3));
-    int ** doublepX = &pX;
+    // int x = 1086;
+    // int *pX = &x;
+    // // printf("asas%d", *pX);
+    // char *p0X = (char*)pX;
+    // // printf("qwqwqw%d", *(p0X+3));
+    // int ** doublepX = &pX;
     // printf("%d", **doublepX);
-    int arr[5] = {1,2,3,4,5};
-    int* pArr = arr;
-    printf("%p\n",arr);
-    printf("%d",arr[3]);
-    printf("%d",*(arr+3));
+    // int arr[5] = {1,2,3,4,5};
+    // int* pArr = arr;
+    // printf("%p\n",arr);
+    // printf("%d",arr[3]);
+    // printf("%d",*(arr+3));
+    int arr[2][3] = {{1,2,3}, {4,5,6}};
+    int (*pArr)[3] = arr;
+    printf("%d\n", **pArr);
+    printf("%d\n", **(pArr+1));
+    printf("%d", *(*pArr+1));
     return 0;
 }
+
+
--- practice/practicalUsagePointers.c
+++ practice/practicalUsagePointers.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
-void randomfunc(int *a){
-    *a = *a + 1;
+ // AS A FUNCTION ARGUMENT TO HAVE A REFERENCE
+// void randomfunc(int *a){
+//     *a = *a + 1;
+// }
+// int main (){
+//     int a = 1;
+//     int *pA = &a; 
+//     randomfunc(pA);
+//     printf("%d", a);
+//     return 0;
+// }
+
+
+// ARRAY AS A FUNCTION ARGUMENT IS POINTER
+void print(char str[]){
+     while(*str!='\0'){
+        printf("%c", *str);
+        str++;
+     }
+}
+int randomfunc(int a[], int size){    // int a[] === int* a 
+    int sum = 0;
+    for(int i = 0; i<size; i++){
+        sum+=a[i];  // a[i] === *(a+i)
+    }
+    return sum;
 }
 int main (){
-    int a = 1;
-    int *pA = &a; 
-    randomfunc(pA);
-    printf("%d", a);
+    int a[] = {1,2,3,4,5}; // int a[] != int* a 
+    int size = sizeof(a)/sizeof(*a);
+    char str[] = "wersa";
+    // randomfunc(a,size);
+    print(str);
+    
     return 0;
 }
+
