af72bc4d4515f67b5d443914de45ca74f102b71f
StasyukAndriy <stasyuk.andrii.b@gmail.com>
2024-06-28 21:03:07 +0300

    func pointers

--- practice/pointers.c
+++ practice/pointers.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-
+int func (int x, int y){
+      return x + y;
+}
 int main (){
-    int x[] = {1,2,3,4,5};
-    int *pX = x;
-    printf("%d", pX[1]);
-    int y = 5;
-    int* p = &y;
-    printf("%d", p[1]);
+    int x = 5;
+    int y = 2;
+    int (*pFunc)(int, int);
+    pFunc = func;
+    printf("%d", pFunc(x,y));
+    // int x[] = {1,2,3,4,5};
+    // int *pX = x;
+    // printf("%d", pX[1]);
+    // int y = 5;
+    // int* p = &y;
+    // printf("%d", p[1]);
     // // printf("asas%d", *pX);
     // char *p0X = (char*)pX;
     // // printf("qwqwqw%d", *(p0X+3));
@@ -23,7 +30,7 @@ int main (){
     // printf("%d\n", **(pArr+1));
     // printf("%d", *(*pArr+1));
     printf("%d", *(arr[1][1]+1)); // 8
-    printf("%d", *(*(*(pArr+1)+1))+1); // 8
+    printf("%d", *(*(*(pArr+1)+1))+1); 
     return 0;
 }
 
